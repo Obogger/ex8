b8b0607da8c9321e85f866c2822282fbc4884491
Sara <sara.nemati@yaelev.se>
2025-02-12 16:42:45 +0100

    Updated headerfile and sourcefile

--- queue.h
+++ queue.h
@@ -18,77 +18,84 @@ class Queue
     {
         T data;
         node_t *next;
-    };    
-    
+    };
+
     IMemory &memory;
     size_t count{0};
     node_t *head{nullptr};
     node_t *tail{nullptr};
 
-    public:
-        Queue(const Queue &) = delete;
-        Queue &operator=(const Queue &) = delete;    Queue(IMemory &_memory) : memory{_memory} {}    Queue(Queue &&that) noexcept : memory(that.memory), count(that.count), head(that.head), tail(that.tail)
+public:
+    Queue(const Queue &) = delete;
+    Queue &operator=(const Queue &) = delete;
+
+    Queue(IMemory &_memory) : memory{_memory} {}
+
+    Queue(Queue &&that) noexcept : memory(that.memory), count(that.count), head(that.head), tail(that.tail)
+    {
+        that.head = nullptr;
+        that.tail = nullptr;
+        that.count = 0;
+    }
+
+    Queue &operator=(Queue &&that) noexcept
+    {
+        if (this != &that)
         {
+            clear();
+            head = that.head;
+            tail = that.tail;
+            count = that.count;
             that.head = nullptr;
             that.tail = nullptr;
             that.count = 0;
-        }    
-
-        Queue &operator=(Queue &&that) noexcept
-        {
-            if (this != &that)
-            {
-                clear();
-                head = that.head;
-                tail = that.tail;
-                count = that.count;
-                that.head = nullptr;
-                that.tail = nullptr;
-                that.count = 0;
-            }
-            return *this;
-        }    
-
-        bool enqueue(const T &item)
-        {
-            node_t *newNode = static_cast<node_t *>(memory.malloc(sizeof(node_t)));
-            if (!newNode)
-                return false;
-            new (newNode) node_t{item, nullptr};
-            if (tail)
-                tail->next = newNode;
-            else
-                head = newNode;
-            tail = newNode;
-            ++count;
-            return true;
-        }    bool dequeue(T &item)
-        {
-            if (!head)
-                return false;
-            node_t *temp = head;
-            item = head->data;
-            head = head->next;
-            if (!head)
-                tail = nullptr;
-            memory.free(temp);
-            --count;
-            return true;
-        }    size_t size(void) const { return count; }    
-        void clear(void)
-        {
-            while (head)
-            {
-                node_t *temp = head;
-                head = head->next;
-                memory.free(temp);
-            }
-            tail = nullptr;
-            count = 0;
-        }    ~Queue()
-        {
-            clear();
         }
+        return *this;
+    }
+
+    bool enqueue(const T &item)
+    {
+        node_t *newNode = static_cast<node_t *>(memory.malloc(sizeof(node_t)));
+        if (!newNode)
+            return false;
+        new (newNode) node_t{item, nullptr};
+        if (tail)
+            tail->next = newNode;
+        else
+            head = newNode;
+        tail = newNode;
+        ++count;
+        return true;
+    }
+    bool dequeue(T &item)
+    {
+        if (!head)
+            return false;
+        node_t *temp = head;
+        item = head->data;
+        head = head->next;
+        if (!head)
+            tail = nullptr;
+        memory.free(temp);
+        --count;
+        return true;
+    }
+    size_t size(void) const { return count; }
+    void clear(void)
+    {
+        while (head)
+        {
+            node_t *temp = head;
+            head = head->next;
+            memory.free(temp);
+        }
+        tail = nullptr;
+        count = 0;
+    }
+    ~Queue()
+    {
+        clear();
+    }
 };
 
-#endif //QUEUE_H
+#endif // QUEUE_H
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "queue.h"
 #include <string>
+#include <tuple>
 #include <cstdint>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -8,50 +9,58 @@ class Memory : public IMemory
 {
     std::vector<void *> vec;
     uint8_t block[8192];
-    public:
-        MOCK_METHOD(void *, malloc, (size_t size), (override));
-        MOCK_METHOD(void, free, (void *ptr), (override));
-        void *allocate(size_t size) 
-        {
-            static size_t pos{0};
 
-            void *ptr{&block[pos]};
+public:
+    MOCK_METHOD(void *, malloc, (size_t size), (override));
+    MOCK_METHOD(void, free, (void *ptr), (override));
+    void *allocate(size_t size)
+    {
+        static size_t pos{0};
 
-            vec.push_back(ptr);
-            pos += size;
+        void *ptr{&block[pos]};
 
-            return ptr;
+        vec.push_back(ptr);
+        pos += size;
 
-        }
+        return ptr;
+    }
 
-        void release(void *ptr)
-        {
-            vec.erase(std::remove(vec.begin(), vec.end(), ptr), vec.end());
-        }
+    void release(void *ptr)
+    {
+        vec.erase(std::remove(vec.begin(), vec.end(), ptr), vec.end());
+    }
 
-        ~Memory() {EXPECT_EQ(0, vec.size()); }
-
+    ~Memory() { EXPECT_EQ(0, vec.size()); }
 };
-
 
 template <typename T>
 class QueueFixture : public ::testing::Test
 {
-    protected:
-        Memory memory;
-        Queue<T> queue{memory};
+    std::tuple<
+        std::vector<int>,
+        std::vector<float>,
+        std::vector<std::string>>
+        allValues{
+            {1, 2, 3, 4, 5},
+            {1.5f, 2.5f, 3.5f, 4.5f, 5.5f},
+            {"aaa", "bbb", "ccc", "ddd", "eee"}};
+
+protected:
+    const std::vector<T> values{std::get<std::vector<T>>(allValues)};
+
+    Memory memory;
+    Queue<T> queue{memory};
+
+    void SetUp(void) override
+    {
+    }
+
+    void TearDown(void) override {}
 };
 
 using Types = ::testing::Types<int, float, std::string>;
 TYPED_TEST_SUITE(QueueFixture, Types);
 
-
-TYPED_TEST(QueueFixture, enqueueAndSize)
-{
-
-}
-
 TYPED_TEST(QueueFixture, empty)
 {
-
 }
