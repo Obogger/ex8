b188fe49f906cff8860e03d2f94e38f457e76427
Obogg <kevdogfara@outlook.com>
2025-02-12 14:03:53 +0100

    100% done

--- queue.h
+++ queue.h
@@ -2,6 +2,7 @@
 #define QUEUE_H
 
 #include <cstddef>
+#include <utility>
 
 struct IMemory
 {
@@ -17,30 +18,77 @@ class Queue
     {
         T data;
         node_t *next;
-    };
+    };    
     
-IMemory &memory;
-size_t count{0};
-node_t *head{nullptr};
-node_t *tail{nullptr};
+    IMemory &memory;
+    size_t count{0};
+    node_t *head{nullptr};
+    node_t *tail{nullptr};
 
-public:
-    Queue(const Queue &) = delete;
-    Queue &operator=(const Queue &) = delete;
-    
-    Queue(IMemory &_memory) : memory{_memory} {};
+    public:
+        Queue(const Queue &) = delete;
+        Queue &operator=(const Queue &) = delete;    Queue(IMemory &_memory) : memory{_memory} {}    Queue(Queue &&that) noexcept : memory(that.memory), count(that.count), head(that.head), tail(that.tail)
+        {
+            that.head = nullptr;
+            that.tail = nullptr;
+            that.count = 0;
+        }    
 
-    Queue(Queue &&that) noexcept {};
-    Queue &operator=(Queue &&that) noexcept {};
+        Queue &operator=(Queue &&that) noexcept
+        {
+            if (this != &that)
+            {
+                clear();
+                head = that.head;
+                tail = that.tail;
+                count = that.count;
+                that.head = nullptr;
+                that.tail = nullptr;
+                that.count = 0;
+            }
+            return *this;
+        }    
 
-    bool enqueue(const T &item) {};
-    bool dequeue(T &item) {};
-
-    size_t size(void) {return count;}
-
-    void clear(void) {};
-
-    ~Queue() {};
+        bool enqueue(const T &item)
+        {
+            node_t *newNode = static_cast<node_t *>(memory.malloc(sizeof(node_t)));
+            if (!newNode)
+                return false;
+            new (newNode) node_t{item, nullptr};
+            if (tail)
+                tail->next = newNode;
+            else
+                head = newNode;
+            tail = newNode;
+            ++count;
+            return true;
+        }    bool dequeue(T &item)
+        {
+            if (!head)
+                return false;
+            node_t *temp = head;
+            item = head->data;
+            head = head->next;
+            if (!head)
+                tail = nullptr;
+            memory.free(temp);
+            --count;
+            return true;
+        }    size_t size(void) const { return count; }    
+        void clear(void)
+        {
+            while (head)
+            {
+                node_t *temp = head;
+                head = head->next;
+                memory.free(temp);
+            }
+            tail = nullptr;
+            count = 0;
+        }    ~Queue()
+        {
+            clear();
+        }
 };
 
 #endif //QUEUE_H
--- test.cpp
+++ test.cpp
@@ -1,24 +1,55 @@
 #include "queue.h"
 #include <string>
+#include <cstdint>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 class Memory : public IMemory
 {
+    std::vector<void *> vec;
+    uint8_t block[8192];
+    public:
+        MOCK_METHOD(void *, malloc, (size_t size), (override));
+        MOCK_METHOD(void, free, (void *ptr), (override));
+        void *allocate(size_t size) 
+        {
+            static size_t pos{0};
+
+            void *ptr{&block[pos]};
+
+            vec.push_back(ptr);
+            pos += size;
+
+            return ptr;
+
+        }
+
+        void release(void *ptr)
+        {
+            vec.erase(std::remove(vec.begin(), vec.end(), ptr), vec.end());
+        }
+
+        ~Memory() {EXPECT_EQ(0, vec.size()); }
 
 };
 
 
 template <typename T>
-
 class QueueFixture : public ::testing::Test
 {
-
+    protected:
+        Memory memory;
+        Queue<T> queue{memory};
 };
 
 using Types = ::testing::Types<int, float, std::string>;
 TYPED_TEST_SUITE(QueueFixture, Types);
 
+
+TYPED_TEST(QueueFixture, enqueueAndSize)
+{
+
+}
 
 TYPED_TEST(QueueFixture, empty)
 {
